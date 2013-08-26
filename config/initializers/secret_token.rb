a6dc8fb5fc7160fe9f0492b253dbecaf56fcb6fe
robodisco <adamsubscribe@googlemail.com>
2013-08-26 15:29:24 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Robodiscofm::Application.config.secret_token = 'd5eec96c6344f13c8c56efb36f57d0c8d4cac05bc55f03beca4dacf8e41e65cc5941968127ef56a149f1565ba2becbd6edb7f4723ae9816da3ff76541dfe4880'
